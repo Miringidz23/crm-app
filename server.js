387ee496bf6e7f91eda6fd63dc3ef564e5e40ed8
Miringidz23 <miringidz23@gmail.com>
2026-09-12 23:53:46 +0100

    Modify CORS settings in server.js
    Updated CORS configuration to allow all origins and simplified allowed methods.

--- server.js
+++ server.js
@@ -79,19 +79,14 @@ function generateSmartReply(messageText) {
 }
 
 const app = express();
-
-// === CORS: السماح لمتجر dzShop بالوصول للـ API ===
+// === السماح لمتجر dzShop بجلب البيانات (CORS) ===
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://miringidz23.github.io");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
   next();
 });
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
